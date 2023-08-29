083d5f038a67d1798865cc1f3c71014122fd9f9b
abhisheksati51 <abhisheksati51@gmail.com>
2023-08-29 17:15:00 +0530

    Do some changes on javascript

--- script.js
+++ script.js
@@ -1,52 +1,157 @@
-// create  a function to convert time to string
+//Initial References
+let timerRef = document.querySelector(".timer-display");
+const hourInput = document.getElementById("hourInput");
+const minuteInput = document.getElementById("minuteInput");
+const activeAlarms = document.querySelector(".activeAlarms");
+const setAlarm = document.getElementById("set");
+let alarmsArray = [];
+let alarmSound = new Audio("Alarm sound.mp3");
 
-const getTimeString = ({hours,minutes,seconds,zone})=>
-{
-    // here this condition is check if minutes/10 is less than 1 than add 0 before the number
-    if(minutes/10<1)
-    {
-        minutes ="0"+minutes;
+let initialHour = 0,
+  initialMinute = 0,
+  alarmIndex = 0;
+
+//Append zeroes for single digit
+const appendZero = (value) => (value < 10 ? "0" + value : value);
+
+//Search for value in object
+const searchObject = (parameter, value) => {
+  let alarmObject,
+    objIndex,
+    exists = false;
+  alarmsArray.forEach((alarm, index) => {
+    if (alarm[parameter] == value) {
+      exists = true;
+      alarmObject = alarm;
+      objIndex = index;
+      return false;
     }
-  // here this condition is check if seconds/10 is less than 1 than add 0 before the number
-    
-  if(seconds/10 < 1)
-    {
-        seconds ="0"+seconds;
-    }
-
-    return `${hours}:${minutes}:${seconds} ${zone}`;
-};
-  
-// function to display curren time on screen
-
-const renderTime =() =>
-{
-    var currentTime = document.getElementById("current-time");
-
-    // create a new object
-
-    const curretDate = new Date();
-
-    var hours = curretDate.getHours();
-    var minutes = curretDate.getMinutes();
-    var seconds = curretDate.getSeconds();
-
-    // We determine if it's "AM" or "PM" based on whether the hours are greater than or equal to 12.
-  
-    var zone = hours >=12 ? "PM":"AM";
-
-     // If the hours are greater than 12, we convert them to a 12-hour format.
-
-    if(hours>=12)
-    {
-        hours = hours%12;
-    }
-    // We're calling another function called "getTimeString" to create a formatted time string
-    const timeString = getTimeString({hours,minutes,seconds,zone});
-    currentTime.innerHTML = timeString;
-
-
+  });
+  return [exists, alarmObject, objIndex];
 };
 
-// We're setting the content of the "currentTime" element to the formatted time string.
-setInterval(renderTime,1000);
+//Display Time
+function displayTimer() {
+  let date = new Date();
+  let [hours, minutes, seconds] = [
+    appendZero(date.getHours()),
+    appendZero(date.getMinutes()),
+    appendZero(date.getSeconds()),
+  ];
+
+  //Display time
+  timerRef.innerHTML = `${hours}:${minutes}:${seconds}`;
+
+  //Alarm
+  alarmsArray.forEach((alarm, index) => {
+    if (alarm.isActive) {
+      if (`${alarm.alarmHour}:${alarm.alarmMinute}` === `${hours}:${minutes}`) {
+        alarmSound.play();
+        alarmSound.loop = true;
+      }
+    }
+  });
+}
+
+const inputCheck = (inputValue) => {
+  inputValue = parseInt(inputValue);
+  if (inputValue < 10) {
+    inputValue = appendZero(inputValue);
+  }
+  return inputValue;
+};
+
+hourInput.addEventListener("input", () => {
+  hourInput.value = inputCheck(hourInput.value);
+});
+
+minuteInput.addEventListener("input", () => {
+  minuteInput.value = inputCheck(minuteInput.value);
+});
+
+//Create alarm div
+
+const createAlarm = (alarmObj) => {
+  //Keys from object
+  const { id, alarmHour, alarmMinute } = alarmObj;
+  //Alarm div
+  let alarmDiv = document.createElement("div");
+  alarmDiv.classList.add("alarm");
+  alarmDiv.setAttribute("data-id", id);
+  alarmDiv.innerHTML = `<span>${alarmHour}: ${alarmMinute}</span>`;
+
+  //checkbox
+  let checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.addEventListener("click", (e) => {
+    if (e.target.checked) {
+      startAlarm(e);
+    } else {
+      stopAlarm(e);
+    }
+  });
+  alarmDiv.appendChild(checkbox);
+  //Delete button
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+  deleteButton.classList.add("deleteButton");
+  deleteButton.addEventListener("click", (e) => deleteAlarm(e));
+  alarmDiv.appendChild(deleteButton);
+  activeAlarms.appendChild(alarmDiv);
+};
+
+//Set Alarm
+setAlarm.addEventListener("click", () => {
+  alarmIndex += 1;
+
+  //alarmObject
+  let alarmObj = {};
+  alarmObj.id = `${alarmIndex}_${hourInput.value}_${minuteInput.value}`;
+  alarmObj.alarmHour = hourInput.value;
+  alarmObj.alarmMinute = minuteInput.value;
+  alarmObj.isActive = false;
+  console.log(alarmObj);
+  alarmsArray.push(alarmObj);
+  createAlarm(alarmObj);
+  hourInput.value = appendZero(initialHour);
+  minuteInput.value = appendZero(initialMinute);
+});
+
+//Start Alarm
+const startAlarm = (e) => {
+  let searchId = e.target.parentElement.getAttribute("data-id");
+  let [exists, obj, index] = searchObject("id", searchId);
+  if (exists) {
+    alarmsArray[index].isActive = true;
+  }
+};
+
+//Stop alarm
+const stopAlarm = (e) => {
+  let searchId = e.target.parentElement.getAttribute("data-id");
+  let [exists, obj, index] = searchObject("id", searchId);
+  if (exists) {
+    alarmsArray[index].isActive = false;
+    alarmSound.pause();
+  }
+};
+
+//delete alarm
+const deleteAlarm = (e) => {
+  let searchId = e.target.parentElement.parentElement.getAttribute("data-id");
+  let [exists, obj, index] = searchObject("id", searchId);
+  if (exists) {
+    e.target.parentElement.parentElement.remove();
+    alarmsArray.splice(index, 1);
+  }
+};
+
+window.onload = () => {
+  setInterval(displayTimer);
+  initialHour = 0;
+  initialMinute = 0;
+  alarmIndex = 0;
+  alarmsArray = [];
+  hourInput.value = appendZero(initialHour);
+  minuteInput.value = appendZero(initialMinute);
+};
